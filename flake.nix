8f12c0f4ac9f12a59b6427df78fd83eb683e8311
Vincent Wong <vincent@vwong.dev>
2025-10-19 01:17:24 +1100

    feat(hm): Add base configuration

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "github.com/V-Wong/home-manager-config";
+
+  inputs.nixpkgs = {
+    url = "github:nixos/nixpkgs/nixpkgs-unstable";  ## Bleeding edge packages
+  };
+
+  inputs.home-manager = {
+    url = "github:nix-community/home-manager/master";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager }: 
+  let
+    username = <FILL_IN>;
+    isDarwin = <FILL_IN>;
+    
+    linuxConfig = {
+      "${username}" = home-manager.lib.homeManagerConfiguration ({
+        modules = [ ./home.nix ];
+        extraSpecialArgs = { username = username; };
+        pkgs = import nixpkgs {
+          system = "x86_64-linux";
+          config.allowUnfree = true;
+        };
+      });
+    };
+    
+    macConfig = {
+      "${username}" = home-manager.lib.homeManagerConfiguration ({
+        modules = [ ./home.nix ];
+        extraSpecialArgs = { username = username; };
+        pkgs = import nixpkgs {
+          system = "aarch64-darwin";
+        };
+      });
+    };
+  in {
+    homeConfigurations = if isDarwin then macConfig else linuxConfig;
+  };
+}
---
+++ home.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, lib, username, ... }:
+let
+  isLinux = pkgs.stdenv.hostPlatform.isLinux;
+  isDarwin = pkgs.stdenv.hostPlatform.isDarwin;
+  unsupported = builtins.abort "Unsupported platform";
+in
+{
+  home.username = username;
+  home.homeDirectory =
+    if isLinux then "/home/${username}" else
+    if isDarwin then "/Users/${username}" else unsupported;
+
+  home.stateVersion = "23.05";
+  programs.home-manager.enable = true;
+
+  home.packages = with pkgs; ([
+    # Platform-agnostic dependencies
+  ] ++ lib.optionals isLinux [
+    # Linux-only dependencies
+  ] ++ lib.optionals isDarwin [
+    # Mac-only dependencies
+  ]);
+
+  # Shell configuration
+  programs.zsh = {
+    enable = true;
+    shellAliases = {
+      hm-switch = "home-manager switch";
+    };
+  };
+}
